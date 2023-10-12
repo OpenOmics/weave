abb180719224478618352548e876b58e9b9707fb
Ryan Routsong <routsongrm@nih.gov>
2023-10-12 10:09:29 -0500

    final ngsqc dockerfile

--- src/Dmux/snk_utils.py
+++ src/Dmux/snk_utils.py
@@ -1,5 +1,4 @@
 def get_adapter_opts(wc, input):
-<<<<<<< HEAD
     adapter_file = open(input.adapters).readlines()
     sid = wc.sid
     sid = '_'.join(sid.split('_')[:-1])
@@ -18,19 +17,3 @@ def get_adapter_opts(wc, input):
     if r2_adapter:
         adapter_flags += f" --adapter_sequence_r2 {r2_adapter}"
     return adapter_flags
-=======
-    r1_index = getr1(wc, input)
-    r2_index = getr2(wc, input)
-    flag = f"--adapter_sequence {r1_index}" 
-    if r2_index not in ('', None):
-        flag += f" --adapter_sequence_r2 {r2_index}"
-    return flag
-
-
-def get_fastp_sample_list(r1, r1_index, r2, r2_index):
-    r1, r2 = open(r1).read().strip(), open(r2).read().strip()
-    if r2 not in ('', None):
-        return [r1, r2]
-    else:
-        return [input.R1]
->>>>>>> 88d43eb (ngsqc fastqc, trimming, fastqc again after trim)
